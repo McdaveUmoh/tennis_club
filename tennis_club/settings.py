cee6458898fb9bc9dbdb08c131b0a8095b769607
“McdaveUmoh” <umohmcdave@gmail.com>
2024-01-23 19:36:53 +0100

    added the details section

--- tennis_club/settings.py
+++ tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&r98ysmpydodsphtz22(#a^#8_xi$5!tq2=4yvfc#9fp$d^601'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
