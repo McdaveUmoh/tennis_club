d55f4d3f792c804c0243b966c8506e552a54fbda
“McdaveUmoh” <umohmcdave@gmail.com>
2024-01-24 22:46:14 +0100

    added new users

--- members/models.py
+++ members/models.py
@@ -7,3 +7,6 @@ class Member(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
--- tennis_club/settings.py
+++ tennis_club/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&r98ysmpydodsphtz22(#a^#8_xi$5!tq2=4yvfc#9fp$d^601'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# for when you want to go live add the domain name here ! ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
