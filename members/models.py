f8c69073fc70fa01510055eb9bdd82fc5519b150
“McdaveUmoh” <umohmcdave@gmail.com>
2024-01-28 14:51:38 +0100

    added in slugs

--- members/models.py
+++ members/models.py
@@ -5,8 +5,9 @@ class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     email = models.EmailField(max_length=100, unique=True)
-    phone = models.IntegerField(null=True)
+    phone = models.BigIntegerField(null=True)
     joined_date = models.DateField(null=True)
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
